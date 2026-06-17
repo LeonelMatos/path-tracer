62a629652ab650b40499b478798f3c4367e38a2c
Leonel Matos <leonellmts@gmail.com>
2026-06-17 15:55:27 +0100

    Added denoiser header

---
+++ src/denoiser.hpp
@@ -0,0 +1,65 @@
+/**
+ * @file denoiser.hpp
+ * @author Leonel Matos
+ * @brief OIDN denoiser
+ * @date 2026-06-17
+ * @copyright Copyright (c) 2026
+ */
+
+#pragma once
+#include <OpenImageDenoise/oidn.hpp>
+#include <vector>
+#include <cstdio>
+
+struct Denoiser {
+    oidn::DeviceRef device;
+    oidn::FilterRef filter;
+    bool initialized = false;
+    int width = 0, height = 0;
+
+    std::vector<float> input_buf;
+    std::vector<float> output_buf;
+
+    void init(int w, int h) {
+        width = w;
+        height = h;
+
+        device = oidn::newDevice();
+        device.commit();
+
+        input_buf.resize(w * h * 3);
+        output_buf.resize(w * h * 3);
+
+        filter = device.newFilter("RT");
+        filter.setImage("color", input_buf.data(), oidn::Format::Float3, w, h);
+        filter.setImage("output", output_buf.data(), oidn::Format::Float3, w, h);
+        filter.set("hdr", true);
+        filter.commit();
+
+        initialized = true;
+        printf("[DENOISER] Initialized %dx%d\n", w, h);
+    }
+
+    void resize(int w, int h) {
+        initialized = false;
+        init(w, h);
+    }
+
+    bool run() {
+        filter.execute();
+        const char* error;
+        if(device.getError(error) != oidn::Error::None) {
+            printf("[DENOISER] Error: %s\n", error);
+            return false;
+        }
+        return true;
+    }
+
+    void cleanup() {
+        filter = oidn::FilterRef();
+        device = oidn::DeviceRef();
+        initialized = false;
+        input_buf.clear();
+        output_buf.clear();
+    }
+};
